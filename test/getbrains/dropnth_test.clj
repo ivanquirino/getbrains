74e23ebc99062f7be2cddf65411d6b8c57e92895
Ivan Quirino <ivan.alencar@invillia.com>
2020-10-03 10:46:33 -0300

    WIP DropNth

--- test/getbrains/dropnth_test.clj
+++ test/getbrains/dropnth_test.clj
@@ -3,7 +3,7 @@
             [getbrains.dropnth :refer :all]))
 
 (deftest dropnth-test
-  (testing "Drop Every Nth Item"
+  (testing "Drop Every Nth Item. Write a function which drops every Nth item from a sequence."
     (is (= (dropnth [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8]))
     (is (= (dropnth [:a :b :c :d :e :f] 2) [:a :c :e]))
     (is (= (dropnth [1 2 3 4 5 6] 4) [1 2 3 5 6]))))
